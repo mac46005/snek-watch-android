a8b46c776081472c6fb9fdbc9cfa4b5e5d81b38a
Marco Preciado <mac46005@gmail.com>
2025-06-01 10:39:37 -0500

    Still making home items. Lets keep at it.

--- app/src/main/java/com/example/snek_watch_android/ui/views/home/components/InfoCard.kt
+++ app/src/main/java/com/example/snek_watch_android/ui/views/home/components/InfoCard.kt
@@ -2,6 +2,9 @@ package com.example.snek_watch_android.ui.views.home.components
 
 import android.view.accessibility.AccessibilityNodeInfo.RangeInfo
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
@@ -10,12 +13,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -36,9 +41,18 @@ fun InfoCard(
         modifier = modifier
     ) {
 //        Spacer(modifier = Modifier.width(300.dp).height(300.dp))
-        Image(
-            painter = painterResource(id = R.drawable.python_1), contentDescription = "Bald"
-        )
+        Box (
+            modifier = Modifier.background(Color.Red, shape = RoundedCornerShape(10.dp))
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.python_1),
+                contentDescription = "Bald",
+                contentScale = ContentScale.FillWidth
+            )
+        }
+
         Column {
 //            Image(painter = painterResource(id = R.), contentDescription = )
             Text(
